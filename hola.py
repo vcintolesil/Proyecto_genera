62519990abcf1bf14721402b68d600cefb03d0b0
Maximilianohub <maximilianotoroojeda@gmail.com>
2016-11-08 08:48:17 -0400

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,1 +1,15 @@
 print "Hola"
+#aporte maximiliano
+#raiz cuadrada
+numero=input("Ingrese numero: ")
+numero=numero*1.0
+if numero>=0:
+    p=numero
+    i=0
+    while i!=p:
+    i=p
+    p=(numero/p+p)/2
+    print "Resultados es: ", p
+
+else :
+print "Numero incorrecto"
