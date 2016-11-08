89437f5a6cfc063e7c45d70529a45c48e020c3b0
Vicky Pizarro <vickypizarro@outlook.com>
2016-11-08 08:41:53 -0400

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,1 +1,20 @@
 print "Hola"
+
+def lcm(x, y):
+   if x > y:
+       mayor = x
+   else:
+       mayor = y
+ 
+   while(True):
+       if((mayor % x == 0) and (mayor % y == 0)):
+           lcm = mayor
+           break
+       mayor += 1
+ 
+   return lcm
+ 
+num1 = int(input("Para calcular el mínimo común múltiplo \n Introduce el primer numero: "))
+num2 = int(input("Introduce el segundo numero: "))
+ 
+print("El minimo comun multiplo de ", num1," y ", num2," es ", lcm(num1, num2))
